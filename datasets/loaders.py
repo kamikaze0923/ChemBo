a9e80e60bc901d440301b19b1199bb37e5b5d321
celsius38 <sx38@cornell.edu>
2019-04-20 14:40:29 -0400

    loader path dynamically determined, new-style class inherit object

--- datasets/loaders.py
+++ datasets/loaders.py
@@ -15,11 +15,15 @@ TODO:
 import numpy as np
 import pandas as pd
 import logging
+import os
 from collections import defaultdict
 from mols.molecule import Molecule
 
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+
 # Class used in CartesianGP
-class MolSampler:
+class MolSampler(object):
     def __init__(self, dataset="chembl", seed=42):
         # load the dataset
         logging.info("Creating a MolSampler")
@@ -36,25 +40,28 @@ class MolSampler:
             ret = list(np.random.choice(self.dataset, n_samples))
         return ret
 
+
 # Helper utilities
 def get_chembl_prop(n_mols=None, as_mols=False):
     """ Returns (pool, smile->prop mappings) """
-    path = "./datasets/ChEMBL_prop.txt"
+    path = os.path.join(__location__, "ChEMBL_prop.txt")
     df = pd.read_csv(path, sep="\t", header=None)
     # smile: v for the first of two properties
     smile_to_prop = {s: v for (s, v) in zip(df[0], df[1])}
     smile_to_prop = defaultdict(int, smile_to_prop)
     return df[0].values, smile_to_prop
 
+
 def get_chembl(n_mols=None, as_mols=True):
     """ Return list of SMILES """
-    path = "./datasets/ChEMBL.txt"
+    path = os.path.join(__location__, "ChEMBL.txt")
     with open(path, "r") as f:
         if n_mols is None:
             res = [line.strip() for line in f]
         else:
             res = [f.readline().strip() for _ in range(n_mols)]
     return [Molecule(smile) for smile in res]
+
 
 def get_initial_pool():
     """Used in chemist_opt.chemist"""
