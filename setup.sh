c816c962d000005ea9052a6cc5722087d574dc8b
celsius38 <sx38@cornell.edu>
2019-05-03 15:02:46 -0400

    default runner for script and formatting of envvar in string

--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
-export PYTHONPATH="$PYTHONPATH:$PWD:$PWD/rdkit_contrib:$PWD/synth/:$PWD/synth/rexgen_direct"
+#!/bin/bash
+export PYTHONPATH="${PYTHONPATH}:${PWD}:${PWD}/rdkit_contrib:${PWD}/synth/:${PWD}/synth/rexgen_direct"
